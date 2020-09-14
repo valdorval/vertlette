50120946ac7f80f35297928f6e298893c2dec0a7
Bhaywe <61256277+Bhaywe@users.noreply.github.com>
2020-09-14 18:50:10 -0400

    Revert "Revert "????""
    This reverts commit bda87fb189e7e99ce8373c2d391017b57dbb7816.

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
 
-function enqueue_styles_vluxe()
-{
-     wp_enqueue_style('style-vluxe', get_template_directory_uri() . '/style.css');
-     wp_enqueue_style('style-principal', get_template_directory_uri() . '/css/main.css');
+function enqueue_styles_vluxe() {
+     wp_enqueue_style('style-vluxe', get_template_directory_uri().'/style.css');
+     wp_enqueue_style('style-principal', get_template_directory_uri().'/css/main.css');
 }
 
+<<<<<<< HEAD
 function vluxe_supports()
 {
      add_theme_support('automatic-feed-links');
@@ -38,6 +38,8 @@ function vluxe_menu_class($classes)
 }
 
 add_action('after_setup_theme', 'vluxe_supports');
+=======
+>>>>>>> parent of 25ffc23... Merge branch 'master' of https://github.com/valdorval/vertlette
 add_action('wp_enqueue_scripts', 'enqueue_styles_vluxe');
-add_filter('nav_menu_css_class', 'vluxe_menu_class', 10, 4);
-//nav_menu_submenu_css_class
+
+// print(get_template_directory_uri().'/css/main.css/');
