67e438de6a455d2b313300807d37e19fccfbabdd
val <vale.dorval@gmail.com>
2020-09-14 15:31:13 -0400

    ajustement index

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
-<!-- <?php
-     get_header();
-     ?>
-<main>
-=======
 <?php
 get_header();
-?>
->>>>>>> master
-
-<?php
 the_content();
-?>
-
-<?php
 get_footer();
-<<<<<<< HEAD
-?> -->
-=======
-?>
->>>>>>> master
