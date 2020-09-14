bda87fb189e7e99ce8373c2d391017b57dbb7816
Bhaywe <61256277+Bhaywe@users.noreply.github.com>
2020-09-14 18:45:43 -0400

    Revert "????"
    This reverts commit 74fcf9c714e1c3ed8eb331f39e7563d4efca1ac8.

--- header.php
+++ header.php
@@ -5,19 +5,19 @@
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <link href="https://fonts.googleapis.com/css2?family=Domine:wght@400;700&family=Rubik:ital,wght@0,300;0,400;0,500;0,700;1,300&display=swap" rel="stylesheet">
-     <title>Vertlette</title>
+     <title>V.Luxe</title>
      <?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class() ?>>
 
      <header class="header">
-          <div class="header__box">
+          <div class="header__box flex">
                <div class="header__logo container-big">
                     <img src="<?php echo get_template_directory_uri(); ?>/img/logo-v-luxe.svg" alt="Image du logo">
                </div>
+               <?php get_search_form() ?>
           </div>
-<<<<<<< HEAD
 
           <nav class="nav" id="main_nav">
                <?php wp_nav_menu([
@@ -26,24 +26,6 @@
                     'menu_class' => 'nav__menu container-big flex center'
                ]); ?>
 
-=======
-          <nav class="nav">
-               <ul class="nav__menu container-big flex">
-                    <li class="nav__menu--item"><a href="#" class="center">Accueil</a></li>
-                    <li class="nav__menu--item"><a href="#" class="center">Boutique</a>
-                         <ul class="nav__submenu">
-                              <li class="nav__submenu--item"><a href="#" class="center">Boutique</a></li>
-                              <li class="nav__submenu--item"><a href="#" class="center">Boutique corporative</a></li>
-                         </ul>
-                    </li>
-                    <li class="nav__menu--item"><a href="#" class="center">Nouvelles</a></li>
-                    <li class="nav__menu--item"><a href="#" class="center">Abonnement</a></li>
-                    <li class="nav__menu--item"><a href="#" class="center">Réparation & Récupération</a></li>
-                    <li class="nav__menu--item"><a href="#" class="center">Live YouTube</a></li>
-                    <li class="nav__menu--item"><a href="#" class="center">Nous contacter</a></li>
-                    <li class="nav__menu--item"><a href="#" class="center">À propos</a></li>
-               </ul>
->>>>>>> parent of 25ffc23... Merge branch 'master' of https://github.com/valdorval/vertlette
           </nav>
           <div class="header__message">
                <p class="center">Livraison gratuite à partir de 2 500$ d'achats</p>
