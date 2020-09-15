47f84a11413785bdd7a0f57723e77b413f91fdaa
Bhaywe <61256277+Bhaywe@users.noreply.github.com>
2020-09-15 12:20:24 -0400

    add nav id and fonctions supports

--- functions.php
+++ functions.php
@@ -7,30 +7,27 @@ function enqueue_styles_vluxe()
 
 function vluxe_supports()
 {
-
-    //     add_theme_support('automatic-feed-links');
-    //     add_theme_support('title-tag');
-    //     add_theme_support('post-thumbnails');
-    //     add_theme_support('menus');
-    register_nav_menus(array(
-        'primary' => __('Navigation en-tête', 'vluxe'),
-    ));
-    //     add_theme_support('html5', array(
-    //         'comment-list',
-    //         'comment-form',
-    //         'search-form',
-    //         'gallery',
-    //         'caption',
-    //         'style',
-    //         'script'
-    //     ));
-}
-
-function mytheme_add_woocommerce_support()
-{
+    add_theme_support('automatic-feed-links');
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('menus');
     add_theme_support('woocommerce');
+    register_nav_menu('main_nav', 'Navigation en-tête');
+    register_nav_menu('footer_nav', 'Pied de page');
+
+    add_theme_support(
+        'html5',
+        [
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+            'style',
+            'script'
+        ]
+    );
 }
-add_action('after_setup_theme', 'mytheme_add_woocommerce_support');
 
 function vluxe_menu_class($classes)
 {
--- header.php
+++ header.php
@@ -17,10 +17,10 @@
                </div>
           </div>
 
-          <nav class="nav" id="main_nav">
+          <nav class="nav">
                <?php wp_nav_menu(
                     array(
-                         'theme_location' => 'primary',
+                         'theme_location' => 'main_nav',
                          'container' => 'ul',
                          'menu_class' => 'nav__menu container-big flex center'
                     )
