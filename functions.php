bd6f83ad3749b9fa20ed788c53204002e0d4ccd3
Bhaywe <61256277+Bhaywe@users.noreply.github.com>
2020-09-15 11:07:18 -0400

    init

--- functions.php
+++ functions.php
@@ -1,27 +1,29 @@
 <?php
 function enqueue_styles_vluxe()
 {
-    wp_enqueue_style('style-vluxe', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('style-principal', get_template_directory_uri() . '/css/main.css');
+    wp_enqueue_style('style-vluxe', get_template_directory_uri() . '/style.css');
 }
 
 function vluxe_supports()
 {
-    add_theme_support('menus');
 
+    //     add_theme_support('automatic-feed-links');
+    //     add_theme_support('title-tag');
+    //     add_theme_support('post-thumbnails');
+    //     add_theme_support('menus');
     register_nav_menus(array(
         'primary' => __('Navigation en-tête', 'vluxe'),
     ));
-
-    // add_theme_support('html5', array(
-    //     'comment-list',
-    //     'comment-form',
-    //     'search-form',
-    //     'gallery',
-    //     'caption',
-    //     'style',
-    //     'script'
-    // ));
+    //     add_theme_support('html5', array(
+    //         'comment-list',
+    //         'comment-form',
+    //         'search-form',
+    //         'gallery',
+    //         'caption',
+    //         'style',
+    //         'script'
+    //     ));
 }
 
 function mytheme_add_woocommerce_support()
