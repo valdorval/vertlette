0ab22a3df563b66820432ed00bc43d7c65d677b8
Bhaywe <61256277+Bhaywe@users.noreply.github.com>
2020-09-16 18:13:08 -0400

    remove space

--- template-parts/promotions.php
+++ template-parts/promotions.php
@@ -18,8 +18,6 @@
                 <img src="<?php echo get_the_post_thumbnail_url($post->ID); ?>" class="custom-class" id="test" />
                 <a href="<?php echo get_permalink(); ?>" title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>">
 
-
-
                     <div class="promo__title">
                         <h4 class="heading-rubik heading-rubik--light"><?php the_title(); ?></h4>
                     </div>
