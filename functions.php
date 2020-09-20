4dd1a6dee5167c21c5e62bd40808e697656093e6
val <vale.dorval@gmail.com>
2020-09-20 17:06:13 -0400

    mon compte et panier

--- functions.php
+++ functions.php
@@ -101,7 +101,7 @@ function vluxe_supports()
     add_theme_support('menus');
     add_theme_support('woocommerce');
     register_nav_menu('main_nav', 'Navigation en-tête');
-    register_nav_menu('footer_nav', 'Pied de page');
+    register_nav_menu('magasin', 'Compte et panier');
 
     add_theme_support(
         'html5',
