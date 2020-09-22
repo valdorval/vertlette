3fcdb4963551c7e6687381fdbf1c49e902385183
Bianka <Biankahale@gmail.com>
2020-09-22 13:59:15 -0400

    remove comment and space

--- custom-page/page-nouvelles.php
+++ custom-page/page-nouvelles.php
@@ -6,123 +6,6 @@
 
 get_header();
 ?>
-<!-- <main>
-    <section class="main-blog">
-        <div class="flex container-big main-blog__article u-margin-top-big">
-            <div class="main-blog__article--text center">
-                <div class="main-blog__center">
-                    <p class="p-primary-light center u-margin-bottom-big">Idées de décoration</p>
-                    <h2 class="heading-tertiary u-margin-bottom-normal">Titre de l'article</h2>
-                    <p class="main-blog__article--description center u-margin-bottom-normal">Description de l’article. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet urna tellus. Nullam imperdiet.</p>
-                    <button class="btn">Lire l'article</button>
-                </div>
-
-            </div>
-            <div class="main-blog__image">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/deco-resize.jpg" alt="Image de l'article">
-            </div>
-        </div>
-    </section>
-
-    <section class="blog-articles u-margin-bottom-normal u-margin-top-big">
-        <div class="container-big flex blog-articles__content">
-            <div class="blog-articles__item u-margin-top-big">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/fauteuil-resize.jpg" alt="Fauteuil">
-                <div class="blog-articles__text">
-                    <h3 class="heading-quaternary"><a href="#">Lorem ipsum</a></h3>
-                    <p class="p-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet urna tellus. Nullam imperdiet.</p>
-                </div>
-            </div>
-            <div class="blog-articles__item u-margin-top-big">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/fauteuil-resize.jpg" alt="Fauteuil">
-                <div class="blog-articles__text">
-                    <h3 class="heading-quaternary"><a href="#">Lorem ipsum</a></h3>
-                    <p class="p-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet urna tellus. Nullam imperdiet.</p>
-                </div>
-            </div>
-            <div class="blog-articles__item u-margin-top-big">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/fauteuil-resize.jpg" alt="Fauteuil">
-                <div class="blog-articles__text">
-                    <h3 class="heading-quaternary"><a href="#">Lorem ipsum</a></h3>
-                    <p class="p-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet urna tellus. Nullam imperdiet.</p>
-                </div>
-            </div>
-            <div class="blog-articles__item u-margin-top-big">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/fauteuil-resize.jpg" alt="Fauteuil">
-                <div class="blog-articles__text">
-                    <h3 class="heading-quaternary"><a href="#">Lorem ipsum</a></h3>
-                    <p class="p-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet urna tellus. Nullam imperdiet.</p>
-                </div>
-            </div>
-            <div class="blog-articles__item u-margin-top-big">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/fauteuil-resize.jpg" alt="Fauteuil">
-                <div class="blog-articles__text">
-                    <h3 class="heading-quaternary"><a href="#">Lorem ipsum</a></h3>
-                    <p class="p-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet urna tellus. Nullam imperdiet.</p>
-                </div>
-            </div>
-            <div class="blog-articles__item u-margin-top-big">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/fauteuil-resize.jpg" alt="Fauteuil">
-                <div class="blog-articles__text">
-                    <h3 class="heading-quaternary"><a href="#">Lorem ipsum</a></h3>
-                    <p class="p-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet urna tellus. Nullam imperdiet.</p>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section class="archives u-margin-bottom-big">
-        <h4 class="container-big heading-secondary heading-secondary--dark u-margin-bottom-normal">Archives</h4>
-        <div class="archives__content">
-            <div class="archives__content--box container-big flex">
-                <div class="archives__content--menu">
-                    <h5 class="heading-rubik--black u-margin-bottom-small">Récents</h5>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                </div>
-                <div class="archives__content--menu">
-                    <h5 class="heading-rubik--black u-margin-bottom-small">Catégories</h5>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                </div>
-                <div class="archives__content--menu">
-                    <h5 class="heading-rubik--black u-margin-bottom-small">Tags</h5>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                </div>
-                <div class="archives__content--menu">
-                    <h5 class="heading-rubik--black u-margin-bottom-small">Mois</h5>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                </div>
-                <div class="archives__content--menu">
-                    <h5 class="heading-rubik--black u-margin-bottom-small">Année</h5>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
-                </div>
-            </div>
-        </div>
-    </section>
-</main> -->
-<!-- 
-<a href="
-<?php
-// the_permalink($the_query->ID) 
-?>
-"> -->
 
 <main>
     <section class="main-blog">
@@ -142,7 +25,6 @@ get_header();
                         <div class="main-blog__center">
                             <!-- deviendrait une categorie -->
                             <p class="p-primary-light center u-margin-bottom-big">Idées de décoration</p>
-
                             <h2 class="heading-tertiary u-margin-bottom-normal"> <?php echo the_title(); ?> </h2>
                             <p class="main-blog__article--description center u-margin-bottom-normal"> <?php echo the_field('description_de_larticle'); ?></p>
                             <a href="<?php the_permalink($the_query->ID) ?>"><button class="btn">Lire l'article</button></a>
@@ -162,7 +44,6 @@ get_header();
         </div>
     </section>
 
-
     <section class="blog-articles u-margin-bottom-normal u-margin-top-big">
         <div class="container-big flex blog-articles__content">
             <?php
@@ -177,8 +58,7 @@ get_header();
 
             if ($args->have_posts()) :
                 while ($args->have_posts()) : $args->the_post();
-                    $feat_image = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
-            ?>
+                    $feat_image = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
                     <div class="blog-articles__item u-margin-top-big">
                         <img src="<?php echo $feat_image; ?>">
                         <div class="blog-articles__text">
