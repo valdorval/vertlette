e07562041b37b3f228153eb77d2f997bbb66178d
Bianka <Biankahale@gmail.com>
2020-09-22 11:57:11 -0400

    j'aime ca quand les merge me delete une page complete :D

---
+++ custom-page/page-nouvelles.php
@@ -0,0 +1,165 @@
+<?php
+
+/**
+ * Template Name: Nouvelles
+ */
+
+get_header();
+?>
+
+<main>
+    <section class="main-blog">
+        <div class="flex container-big main-blog__article u-margin-top-big">
+            <div class="main-blog__article--text center">
+                <div class="main-blog__center">
+                    <p class="p-primary-light center u-margin-bottom-big">Idées de décoration</p>
+                    <h2 class="heading-tertiary u-margin-bottom-normal">Titre de l'article</h2>
+                    <p class="main-blog__article--description center u-margin-bottom-normal">Description de l’article. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet urna tellus. Nullam imperdiet.</p>
+                    <button class="btn">Lire l'article</button>
+                </div>
+
+            </div>
+            <div class="main-blog__image">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/deco-resize.jpg" alt="Image de l'article">
+            </div>
+        </div>
+    </section>
+
+    <section class="blog-articles u-margin-bottom-normal u-margin-top-big">
+        <div class="container-big flex blog-articles__content">
+            <div class="blog-articles__item u-margin-top-big">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/fauteuil-resize.jpg" alt="Fauteuil">
+                <div class="blog-articles__text">
+                    <h3 class="heading-quaternary"><a href="#">Lorem ipsum</a></h3>
+                    <p class="p-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet urna tellus. Nullam imperdiet.</p>
+                </div>
+            </div>
+            <div class="blog-articles__item u-margin-top-big">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/fauteuil-resize.jpg" alt="Fauteuil">
+                <div class="blog-articles__text">
+                    <h3 class="heading-quaternary"><a href="#">Lorem ipsum</a></h3>
+                    <p class="p-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet urna tellus. Nullam imperdiet.</p>
+                </div>
+            </div>
+            <div class="blog-articles__item u-margin-top-big">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/fauteuil-resize.jpg" alt="Fauteuil">
+                <div class="blog-articles__text">
+                    <h3 class="heading-quaternary"><a href="#">Lorem ipsum</a></h3>
+                    <p class="p-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet urna tellus. Nullam imperdiet.</p>
+                </div>
+            </div>
+            <div class="blog-articles__item u-margin-top-big">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/fauteuil-resize.jpg" alt="Fauteuil">
+                <div class="blog-articles__text">
+                    <h3 class="heading-quaternary"><a href="#">Lorem ipsum</a></h3>
+                    <p class="p-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet urna tellus. Nullam imperdiet.</p>
+                </div>
+            </div>
+            <div class="blog-articles__item u-margin-top-big">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/fauteuil-resize.jpg" alt="Fauteuil">
+                <div class="blog-articles__text">
+                    <h3 class="heading-quaternary"><a href="#">Lorem ipsum</a></h3>
+                    <p class="p-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet urna tellus. Nullam imperdiet.</p>
+                </div>
+            </div>
+            <div class="blog-articles__item u-margin-top-big">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/fauteuil-resize.jpg" alt="Fauteuil">
+                <div class="blog-articles__text">
+                    <h3 class="heading-quaternary"><a href="#">Lorem ipsum</a></h3>
+                    <p class="p-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet urna tellus. Nullam imperdiet.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="archives u-margin-bottom-big">
+        <h4 class="container-big heading-secondary heading-secondary--dark u-margin-bottom-normal">Archives</h4>
+        <div class="archives__content">
+            <div class="archives__content--box container-big flex">
+                <div class="archives__content--menu">
+                    <h5 class="heading-rubik--black u-margin-bottom-small">Récents</h5>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                </div>
+                <div class="archives__content--menu">
+                    <h5 class="heading-rubik--black u-margin-bottom-small">Catégories</h5>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                </div>
+                <div class="archives__content--menu">
+                    <h5 class="heading-rubik--black u-margin-bottom-small">Tags</h5>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                </div>
+                <div class="archives__content--menu">
+                    <h5 class="heading-rubik--black u-margin-bottom-small">Mois</h5>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                </div>
+                <div class="archives__content--menu">
+                    <h5 class="heading-rubik--black u-margin-bottom-small">Année</h5>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                    <p class="u-margin-bottom-xsmall"><a href="#">Lorem ipsum dolor sit amet</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<div>
+    <p> ---------recent post-------</p>
+    <?php
+    // Affiche le dernier post publié
+    $the_query = new WP_Query(array(
+        'post_type' => 'nouvelles',
+        'posts_per_page' => 1,
+    ));
+
+    if ($the_query->have_posts()) :
+        while ($the_query->have_posts()) : $the_query->the_post();
+    ?>
+            <h2> <?php echo the_title(); ?> </h2>
+            <p> <?php echo the_content(); ?></p>
+    <?php endwhile;
+        wp_reset_postdata();
+    else :
+    endif;
+    ?>
+    <p> ---------recent post-------</p>
+</div>
+
+<?php
+// affiche tout les post exepté le dernier publié avec offset
+$args = new WP_Query(array(
+    'post_type'             => 'nouvelles',
+    'post_per_page'         =>  6,
+    'ignore_sticky_posts'   => 1,
+    'paged'                 => $paged,
+    'offset'                => 1,
+));
+
+if ($args->have_posts()) :
+    while ($args->have_posts()) : $args->the_post();
+?>
+        <h2> <?php echo the_title(); ?> </h2>
+        <p> <?php echo the_content(); ?></p>
+        <h6> <?php echo the_author(); ?></h6>
+<?php
+    endwhile;
+endif;
+
+get_footer();
+?>
