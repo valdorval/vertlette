545e99c1eb038c7d619cc84b5392e99d05a28953
Bianka <Biankahale@gmail.com>
2020-09-22 16:16:09 -0400

    css about

--- template-parts/about.php
+++ template-parts/about.php
@@ -1,9 +1,9 @@
-<main>
-     <section class="about .u-margin-top-big gradient">
+<main class="gradient">
+     <section class="about u-margin-top-big u-padding-bottom-big">
           <div class="container">
-               <h1 class="heading-tertiary u-margin-top-big u-margin-bottom-normal"><?php echo get_field('about_title'); ?></h1>
+               <h1 class="heading-tertiary u-margin-top-big u-margin-bottom-xsmall"><?php echo get_field('about_title'); ?></h1>
                <p><?php echo get_field('about_p'); ?></p>
-               <h2 class="heading-tertiary u-margin-bottom-normal"><?php echo get_field('second_about_title'); ?></h2>
+               <h2 class="heading-tertiary u-margin-bottom-xsmall u-margin-top-normal"><?php echo get_field('second_about_title'); ?></h2>
                <p><?php echo get_field('second_about_p'); ?></p>
           </div>
      </section>
