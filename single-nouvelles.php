4fd65842f0124e09d1291196b0165a04f14e653b
Bianka <Biankahale@gmail.com>
2020-09-22 14:56:07 -0400

    single article css done

--- single-nouvelles.php
+++ single-nouvelles.php
@@ -3,12 +3,25 @@ get_header();
 
 global $post;
 $author_id = $post->post_author;
+
+$image = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
 ?>
+<main>
+    <article class="article">
+        <h1 class="u-margin-top-big"><?php echo the_title(); ?></h1>
+        <p class="article--description"><?php the_field('description_de_larticle') ?></p>
 
-<h2><?php echo the_title(); ?></h2>
-<p> <?php echo the_content(); ?></p>
-<h6> <?php the_author_meta('user_nicename', $author_id);; ?></h6>
+        <div class="article__header">
+            <img src="<?php echo $image; ?>" alt="Image de l'article">
+        </div>
 
+        <p class="article__content"> <?php echo the_content(); ?></p>
+        <div class="flex auteur u-margin-top-big u-margin-bottom-big">
+            <span>Auteur:</span>
+            <p> <?php the_author_meta('user_nicename', $author_id);; ?></p>
+        </div>
+    </article>
+</main>
 
 <?php
 get_footer();
