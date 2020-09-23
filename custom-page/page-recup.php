99add1d39a4dbbc6bb9eb44d8ee83264b2299689
Bianka <Biankahale@gmail.com>
2020-09-23 11:12:03 -0400

    page repuc supprimer les p en trop

--- custom-page/page-recup.php
+++ custom-page/page-recup.php
@@ -9,12 +9,12 @@ get_header();
 ?>
 
 <main>
-     <h1 class="heading-tertiary u-margin-bottom-big u-margin-top-big center">Les services que nous offrons</h1>
+     <h1 class="u-margin-bottom-big u-margin-top-big center">Les services que nous offrons</h1>
      <section class="reparation u-margin-top-big container-big">
-          <h2 class="u-margin-bottom-normal">Réparation</h2>
+          <h2 class="heading-black u-margin-bottom-normal">Réparation</h2>
           <div class="flex reparation__content u-margin-bottom-normal">
                <div class="reparation__text">
-                    <p><?php echo get_field('reparation'); ?></p>
+                    <?php echo get_field('reparation'); ?>
                </div>
                <div class="reparation__img center">
                     <img src="<?php echo get_template_directory_uri(); ?>/img/renovation.jpg" alt="Renovation">
@@ -23,13 +23,13 @@ get_header();
           <button class="btn btn__dark u-margin-top-small"><a href="<?php echo get_permalink('35'); ?>">Nous contacter</a></button>
      </section>
      <section class="recup u-margin-top-big container-big">
-          <h2 class="u-margin-bottom-normal">Récupération</h2>
+          <h2 class="heading-black u-margin-bottom-normal">Récupération</h2>
           <div class="flex recup__content u-margin-bottom-normal">
                <div class="recup__img center">
                     <img src="<?php echo get_template_directory_uri(); ?>/img/chaise-ancienne.jpg" alt="Chaise">
                </div>
                <div class="recup__text">
-                    <p><?php echo get_field('recuperation'); ?></p>
+                    <?php echo get_field('recuperation'); ?>
                </div>
           </div>
           <div class="recup__button-right">
