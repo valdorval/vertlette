b4a71c3934ef4a1daf94a3c5cf9f6867d850ab55
val <vale.dorval@gmail.com>
2020-09-24 13:51:12 -0400

    recherche ok

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
-<form class="search-form" action="<?php echo esc_url(home_url('/')) ?>">
-     <input type="search" class="search-form__input" placeholder="Rechercher" name="s" value="<?php echo get_search_query(); ?>">
+<form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url('/')); ?>">
+     <input type="text" class="search-form__input" placeholder="Rechercher" name="s" value="<?php get_search_query(); ?>">
 
      <div class="search-form__loupe">
           <img src="<?php echo get_template_directory_uri(); ?>/img/loupe.svg" alt="Loupe">
