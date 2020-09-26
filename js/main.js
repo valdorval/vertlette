9e1f14209e907756e64373f4659735b2c2b5529c
val <vale.dorval@gmail.com>
2020-09-25 20:08:54 -0400

    res nouvelles

--- js/main.js
+++ js/main.js
@@ -7,4 +7,14 @@ $(function () {
 
      closeItem($('#messageClose'), $('.header__message'));
 
+     console.log($('.input-text').value);
+     $('#dec-button').click(function () {
+          $('.qty').value = parseInt($('.qty').value) - 1;
+     })
+
+     $('#inc-button').click(function () {
+          $('.qty input').value = parseInt($('.qty input').value) + 1;
+     })
+
+
 });
